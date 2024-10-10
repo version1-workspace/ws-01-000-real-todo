20e2bfac9fdb02b3bd7464019acbd7a33ced6772
Jiro <j.code.public@gmail.com>
2024-10-10 11:02:22 -0700

    Style Login Page

--- frontend/core/src/components/shared/header/public/index.tsx
+++ frontend/core/src/components/shared/header/public/index.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import Button from "@/components/shared/button";
 import Image from "next/image";
 import logo from "@/assets/logo.png";
+import { classHelper } from "@/lib/cls";
 
 interface Props {
   light?: boolean;
@@ -18,15 +19,32 @@ export default function Header({ light }: Props) {
               styles.logoContent,
               light ? styles.logoContentLight : "",
             ].join(" ")}>
-            <Image src={logo} alt="ロゴ" />
-            <h2>Turvo</h2>
+            <Image
+              width={24}
+              className={styles.logoImage}
+              src={logo}
+              alt="ロゴ"
+            />
+            <h2 className={styles.logoText}>Turvo</h2>
           </div>
         </Link>
       </div>
       <div className={styles.right}>
         <ul className={styles.menu}>
-          <li className={styles.menuItem}>Turvo について</li>
-          <li className={styles.menuItem}>ドキュメント</li>
+          <li
+            className={classHelper({
+              [styles.menuItem]: true,
+              [styles.menuItemLight]: light,
+            })}>
+            Turvo について
+          </li>
+          <li
+            className={classHelper({
+              [styles.menuItem]: true,
+              [styles.menuItemLight]: light,
+            })}>
+            ドキュメント
+          </li>
         </ul>
         <div className={styles.actions}>
           <Link className={styles.signup} href="/login">
