d75020a71a0351a2e92b36a645f56e091cab3cf1
Jiro <j.code.public@gmail.com>
2025-12-07 18:48:50 +0900

    useFilter をリファクタ

--- frontend/core/src/components/tasks/list/hooks/useFilter.ts
+++ frontend/core/src/components/tasks/list/hooks/useFilter.ts
@@ -1,4 +1,4 @@
-import { useState, useMemo } from "react";
+import { useEffect, useState, useMemo } from "react";
 import { useSearchParams } from "next/navigation";
 import { QueryString } from "@/lib/queryString";
 
@@ -28,39 +28,6 @@ export interface Params {
   limit: number;
   page: number;
 }
-
-interface UrlObject {
-  page: number;
-  projectId?: string;
-  status: string[];
-  limit: number;
-  search: string;
-  sortType: string;
-  sortOrder: string;
-  dateFrom: string;
-  dateTo: string;
-  dateType: string;
-}
-
-const convertFilterParamsToUrlObject = (params: Params): Partial<UrlObject> => {
-  const result: Partial<UrlObject> = {
-    page: params.page || 1,
-    projectId: params.projectId,
-    search: params.text || "",
-    limit: params.limit,
-    status: Object.keys(params.statuses).filter((key) => params.statuses[key]),
-    sortType: params.order.type,
-    sortOrder: params.order.value,
-  };
-
-  if (params.date.start || params.date.end) {
-    result.dateType = params.date.type;
-    result.dateFrom = params.date.start || "";
-    result.dateTo = params.date.end || "";
-  }
-
-  return result;
-};
 
 export type FieldTypes = keyof typeof Fields;
 export type OrderType = "asc" | "desc";
@@ -100,12 +67,11 @@ const mergeValues = (base: Params, obj: Partial<Params>) => {
 
   return result;
 };
+
 export interface Filter {
-  ready: boolean;
   original: Params;
   replica: Params;
   isDateSet: boolean;
-  searchURI: string;
   update: (_params: Params) => void;
   save: (_value?: Params) => void;
   reset: () => void;
@@ -113,77 +79,43 @@ export interface Filter {
 }
 
 export default function useFilter(): Filter {
-  const [filterValues, setFilterValues] = useState<{
-    original: Params;
-    replica: Params;
-  }>({
-    original: initialValue,
-    replica: initialValue,
-  });
   const searchParams = useSearchParams();
 
   const qs = useMemo(() => new QueryString(searchParams), [searchParams]);
-
-  const base = useMemo(() => {
-    const merged = mergeValues(initialValue, qs.object);
-    return {
-      original: merged,
-      replica: JSON.parse(JSON.stringify(merged)),
-    };
+  const original = useMemo(() => {
+    return mergeValues(initialValue, qs.object);
   }, [qs]);
 
+  const [replica, setReplica] = useState<Params>(original);
   const update = (params: Params) => {
-    setFilterValues({
-      ...filterValues,
-      replica: params,
-    });
+    setReplica(params);
   };
 
   const save = (value?: Params) => {
-    const newOriginal = value || filterValues.replica;
-    setFilterValues({
-      ...filterValues,
-      original: newOriginal,
-    });
+    const newOriginal = value || replica;
 
-    const urlObject = convertFilterParamsToUrlObject(newOriginal);
-    const newQs = new QueryString(urlObject);
+    const newQs = new QueryString(newOriginal);
 
     history.replaceState(null, "", "?" + newQs.toString());
   };
 
   const reset = () => {
-    setFilterValues({
-      ...filterValues,
-      replica: filterValues.original,
-    });
+    setReplica(original);
   };
 
   const resetState = (type: keyof Params) => {
-    const newValue = {
-      ...filterValues,
+    const newReplica = {
+      ...replica,
       [type]: { ...initialValue }[type],
     };
-
-    if (type === "text") {
-      newValue.original.statuses = { ...initialValue.statuses };
-      newValue.replica.statuses = { ...initialValue.statuses };
-    }
-
-    setFilterValues(newValue);
-    history.replaceState(null, "", "?" + qs.toString());
-
-    return newValue.replica;
+    save(newReplica);
+    return newReplica;
   };
 
-  const { original } = filterValues;
-
   return {
-    ready: !!qs,
-    original: base.original,
-    replica: base.replica,
+    original, // 保存された状態
+    replica, // 編集中の状態
     isDateSet: !!(original.date.start || original.date.end),
-    searchURI: qs?.toString() || "",
     update,
     save,
     reset,
